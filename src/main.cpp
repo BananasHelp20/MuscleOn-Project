8eb40917841ecb1e32ca1c7318b025810fad410f
20230018 <20230018@htl-perg.ac.at>
2026-01-08 08:25:52 +0100

    connected esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#define EMG_PIN 34
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  analogReadResolution(12);        // 0–4095
+  analogSetAttenuation(ADC_11db);  // up to ~3.3V
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int raw = analogRead(EMG_PIN);
+  float voltage = raw * (3.3 / 4095.0);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Raw: ");
+  Serial.print(raw);
+  Serial.print("  Voltage: ");
+  Serial.println(voltage, 3);
+
+  delay(10);
 }
