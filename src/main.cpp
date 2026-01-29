658d919ea670256ec70f6df3b4039e507053c39d
tobias.winkler <20230066@students.htl-perg.ac.at>
2026-01-29 09:01:07 +0100

    add bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
 #include "calibrations.h"
+#include <BluetoothSerial.h>
 
 static int iteration = 0;
 static float smoothedValue = 0.0;
 static float baseline = 0.0;
 static float maxValue = 0.0;
+BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
+  SerialBT.begin("ESP32_EMG");
+  
   analogReadResolution(12);        // 0–4095
   analogSetAttenuation(ADC_11db);  // up to ~3.3V
+
+  Serial.println("ESP32 ready");
+  SerialBT.println("ESP32 ready");
 }
 
 
@@ -21,6 +28,7 @@ void loop() {
   smoothedValue = smooth(smoothedValue, raw, 0.1);
 
   
+
   if (!started) {
     if (smoothedValue > 600.0) {
       baseline = generateBaseLine();
@@ -37,10 +45,10 @@ void loop() {
 }
 
 void printValues() {
-  Serial.print("Raw (smoothed): ");
-  Serial.print(smoothedValue, 0);
-  Serial.print(" | Baseline-Adjusted: ");
-  Serial.print(smoothedValue - baseline, 0);
-  Serial.print(" | Normalized: ");
-  Serial.println((smoothedValue - baseline) / (maxValue - baseline), 2);
+  SerialBT.print("Raw (smoothed): ");
+  SerialBT.print(smoothedValue, 0);
+  SerialBT.print(" | Baseline-Adjusted: ");
+  SerialBT.print(smoothedValue - baseline, 0);
+  SerialBT.print(" | Normalized: ");
+  SerialBT.println((smoothedValue - baseline) / (maxValue - baseline), 2);
 }
