bc636182ed6f9f419ec26f5215bf24a3e2b0d2bb
20230036 <20230036@students.htl-perg.ac.at>
2026-06-23 13:53:34 +0200

    adf

--- Website/backend/router/muscleon.router.ts
+++ Website/backend/router/muscleon.router.ts
@@ -5,27 +5,27 @@ import * as model from '../model/muscleon.model';
 import { sendMail, validateMail } from '../mail/muscleon.mail';
 export let muscleRouter = express.Router();
 
-muscleRouter.get('/getUserData', async (req, res) => {
+muscleRouter.get('/getUserData', async (req, res) => { //Userdaten aus den JSON files holen und zurücksenden, wenn user ned gefunden wurde, ein dem entsprechendes objekt answer zurücksenden { userid=-1, und userName="", found=false}
     res.statusCode = 200;
     res.send(await gatherUserData());
 });
 
-muscleRouter.get('/getTasks', async (req, res) => {
+muscleRouter.get('/getTasks', async (req, res) => { //Tasks aus den JSON files holen und zurücksenden.
     res.statusCode = 200;
     res.send(await getTasks());
 });
 
-muscleRouter.get('/getSupportedExercises', async (req, res) => {
+muscleRouter.get('/getSupportedExercises', async (req, res) => { //SupportedExercises aus den JSON files holen und zurücksenden.
     res.statusCode = 200;
     res.send(await gatherSupportedExercises());
 });
 
-muscleRouter.get('/getDeviceData', async (req, res) => {
+muscleRouter.get('/getDeviceData', async (req, res) => { //DeviceData aus den JSON files holen und zurücksenden.
     res.statusCode = 200;
     res.send(await gatherDeviceData());
 });
 
-muscleRouter.post('/setUserData', async (req, res) => {
+muscleRouter.post('/setUserData', async (req, res) => { //Nutzerdaten empfangen und lokale files überschreiben, wenn user ned gefunen wurde, wird ein error geworfen
     await setUserData(req.body).catch((err) => {
         console.error("Error writing user data:", err);
         res.statusCode = 500;
@@ -36,7 +36,7 @@ muscleRouter.post('/setUserData', async (req, res) => {
     res.send({ message: "User data updated successfully" });
 });
 
-muscleRouter.post('/setUserSettings', async (req, res) => {
+muscleRouter.post('/setUserSettings', async (req, res) => { //Quasi selbes wie setUserData, nur spezifisch für settings
     await setUserSettings(req.body).catch((err) => {
         console.error("Error writing user settings:", err);
         res.statusCode = 500;
@@ -47,7 +47,7 @@ muscleRouter.post('/setUserSettings', async (req, res) => {
     res.send({ message: "User settings updated successfully" });
 });
 
-muscleRouter.post('/setUserProperties', async (req, res) => {
+muscleRouter.post('/setUserProperties', async (req, res) => { //selbes prinzip wie setUserSettings
     await setUserProperties(req.body).catch((err) => {
         console.error("Error writing user propertis:", err);
         res.statusCode = 500;
@@ -58,7 +58,7 @@ muscleRouter.post('/setUserProperties', async (req, res) => {
     res.send({ message: "User properties updated successfully" });
 });
 
-muscleRouter.post('/setDeviceData', async (req, res) => {
+muscleRouter.post('/setDeviceData', async (req, res) => { //selbes wie setUserSettings, nur für DeviceData
     await setDeviceData(req.body).catch((err) => {
         console.error("Error writing device data:", err);
         res.statusCode = 500;
@@ -69,9 +69,14 @@ muscleRouter.post('/setDeviceData', async (req, res) => {
     res.send({ message: "Device data updated successfully" });
 });
 
-muscleRouter.post('/loadUserData', async (req, res) => {
+muscleRouter.post('/loadUserData', async (req, res) => { //Dies ist wohl eine eher komplexe Funktion:
+    /**
+     * Hier werden alle lokalen JSON files die den momentanen Nutzer betreffen aus der Datenbank geladen.
+     * Damit werden die lokalen files überschrieben, und in currentProerties.json wird die userId wie auch der name des momentanen Nutzers gespeichert.
+     * 
+     */
     // daten von da Datenbank holen und in de JSON dateien schreiben, wenn user ned gefunden wurde, ein dem entsprechendes objekt answer zurücksenden { userid=-1, und userName="", found=false}
-    let data = await gatherUserData(); //provisorisch
+    let data = await gatherUserData(); //kann erst gemacht werden, wenn alle Files fertig beschrieben wurden.
     let answer: model.DatabaseAnswer = {
         found: true,
         userId: data.userProperties.userId,
@@ -84,9 +89,14 @@ muscleRouter.post('/loadUserData', async (req, res) => {
     res.send(answer);
 });
 
-muscleRouter.post('/loadUserDataById', async (req, res) => {
+muscleRouter.post('/loadUserDataById', async (req, res) => { //gleiches wie loadUserData, nur diesmal basierend auf der ID
+    /**
+     * Hier werden alle lokalen JSON files die den momentanen Nutzer betreffen aus der Datenbank geladen.
+     * Damit werden die lokalen files überschrieben, und in currentProerties.json wird die userId wie auch der name des momentanen Nutzers gespeichert.
+     * 
+     */
     // daten von da Datenbank holen und in de JSON dateien schreiben, wenn user ned gefunden wurde, ein dem entsprechendes objekt answer zurücksenden { userid=-1, und userName="", found=false}
-    let data = await gatherUserData(); //provisorisch
+    let data = await gatherUserData(); //kann erst gemacht werden, wenn alle Files fertig beschrieben wurden.
     let answer: model.DatabaseAnswer = {
         found: true,
         userId: data.userProperties.userId,
@@ -99,7 +109,7 @@ muscleRouter.post('/loadUserDataById', async (req, res) => {
     res.send(answer);
 });
 
-muscleRouter.post('/clearUserData', async (req, res) => {
+muscleRouter.post('/clearUserData', async (req, res) => { //Alle JSON files die den Nutzer betreffen werden gecleared bzw zurückgesetzt, also bei den meisten auf "[]" oder "{}" gsetzt, dies zeigt, das momentan niemand eingeloggt ist.
     await clearUserData(req.body).catch((err) => {
         console.error("Error clearing data:", err);
         res.statusCode = 500;
@@ -111,14 +121,16 @@ muscleRouter.post('/clearUserData', async (req, res) => {
 });
 
 muscleRouter.post('/addUser', async (req, res) => {
-    //in req.body san de UserDaten gespeichert, de sollten glei in de Datenbank gespeichert werdnen
-    //hier wird dem Nutzer eine freie UserId (Zahl) zugewiesen.
-    //Die daten werden direkt in alle Nutzerbetreffenden JSON files geladen, indem vorhandenes in den JSON files in die Datenbank gesichert (basierend auf userid) und dann überschrieben wird.
+    /*
+    Wenn ein nutzer erstellt wird, werden die Daten des nutzers aus req.body ausgelesen. Dann wird in der Datenbank ein Nutzer angelegt mit diesen Daten.
+    Von dem neuen Datenbank nutzer wird dann die ID abgefragt, da diese bis zu diesem Zeitpunkt noch nicht feststeht.
+    Danach wird loadDatabyuserID mit dieser ID aufgerufen, um die Daten des neuen Nutzers in die JSON files zu laden.
+    */
     res.statusCode = 200;
     res.send({ message: "creating user successful!", created: true }); //created sollte false sein wenn fehler aufgetreten sind.
 });
 
-muscleRouter.post('/deleteUser', async (req, res) => {
+muscleRouter.post('/deleteUser', async (req, res) => {// Nutzer in der Datenbank löschen, danach JSON files zurücksetzen.
     let answer: model.DatabaseAnswer = { //Daten des gelöschten Users zurückgeben, dies sind testdaten
         found: true,
         userId: 0,
@@ -130,12 +142,12 @@ muscleRouter.post('/deleteUser', async (req, res) => {
     res.send(answer);
 });
 
-muscleRouter.post('/getUser/byId', async (req, res) => {
+muscleRouter.post('/getUser/byId', async (req, res) => { //sucht Nutzer in der Datenbank
     let userIdOfUser: {userId: string | number} = req.body; //such in user aus da Datenbank via id, und sende a databaseAnswer zurück, wie bei deleteUser z.B., do schreibst du dann einfach des komplette Userobjekt in answer.userProperties eini.
     res.sendStatus(200); //provisorisch
 });
 
-muscleRouter.post("/saveExercise", async (req, res) => {
+muscleRouter.post("/saveExercise", async (req, res) => { // Exercise speichern
     let exercises: { oldExercise: model.Exercise; newExercise: model.Exercise } = req.body;
     await saveExercise(exercises.oldExercise, exercises.newExercise).catch((err) => {
         console.error("Error deleting Exercise:", err);
@@ -147,7 +159,7 @@ muscleRouter.post("/saveExercise", async (req, res) => {
     res.send({ message: "deleting Exercise successful!" });
 });
 
-muscleRouter.post("/saveTasks", async (req, res) => {
+muscleRouter.post("/saveTasks", async (req, res) => { // tasks speichern
     let tasks: string[][] = req.body;
     await saveTasks(tasks).catch((err) => {
         console.error("Error saving tasks:", err);
@@ -159,7 +171,7 @@ muscleRouter.post("/saveTasks", async (req, res) => {
     res.send({ message: "saving tasks successful!" });
 });
 
-muscleRouter.post('/deleteExercise', async (req, res) => {
+muscleRouter.post('/deleteExercise', async (req, res) => { // exercise löschen
     let exerciseToDelete: { name: string } = req.body;
     await deleteExercise(exerciseToDelete.name).catch((err) => {
         console.error("Error deleting Exercise:", err);
@@ -171,7 +183,7 @@ muscleRouter.post('/deleteExercise', async (req, res) => {
     res.send({ message: "deleting Exercise successful!" });
 });
 
-muscleRouter.post('/saveTimesNOPE', async (req, res) => {
+muscleRouter.post('/saveTimesNOPE', async (req, res) => { //ghostcode bzw gerade nicht verwendet
     await saveTrainingsPlan(req.body).catch((err) => {
         console.error("Error clearing data:", err);
         res.statusCode = 500;
@@ -183,22 +195,22 @@ muscleRouter.post('/saveTimesNOPE', async (req, res) => {
 });
 
 
-muscleRouter.get('/getExercises/supported', async (req, res) => {
+muscleRouter.get('/getExercises/supported', async (req, res) => { //supported exercises holen
     res.statusCode = 200;
     res.send(await gatherSupportedExercises());
 });
 
-muscleRouter.get('/getExercises/unsupported', async (req, res) => {
+muscleRouter.get('/getExercises/unsupported', async (req, res) => { //unsupportet exercises holen
     res.statusCode = 200;
     res.send(await gatherUnsupportedExercises());
 });
 
-muscleRouter.get('/getExercises/user', async (req, res) => {
+muscleRouter.get('/getExercises/user', async (req, res) => { // userdefined exercises holen
     res.statusCode = 200;
     res.send(await gatherUserExercises());
 });
 
-muscleRouter.get('/session/start', async (req, res) => {
+muscleRouter.get('/session/start', async (req, res) => { //session starten (auskommentierter code ist noch nicht implementiert momentan)
     // await startSession().catch((err) => {
     //     console.error("Error starting session", err);
     //     res.statusCode = 500;
@@ -213,7 +225,7 @@ muscleRouter.get('/session/start', async (req, res) => {
     });
 });
 
-muscleRouter.get("/exercise/start", async (req, res) => {
+muscleRouter.get("/exercise/start", async (req, res) => { // exercise starten (auskommentierter code ist noch nicht implementiert momentan)
     // await startExercise().catch((err) => {
     //     console.error("Error starting exercise:", err);
     //     res.statusCode = 500;
@@ -227,7 +239,7 @@ muscleRouter.get("/exercise/start", async (req, res) => {
     });
 });
 
-muscleRouter.get("/exercise/skip", async (req, res) => {
+muscleRouter.get("/exercise/skip", async (req, res) => {// exercise skippen (auskommentierter code ist noch nicht implementiert momentan)
     // await skipExercise().catch((err) => {
     //     console.error("Error skipping exercise:", err);
     //     res.statusCode = 500;
@@ -241,7 +253,7 @@ muscleRouter.get("/exercise/skip", async (req, res) => {
     });
 });
 
-muscleRouter.post('/session/stop', async (req, res) => {
+muscleRouter.post('/session/stop', async (req, res) => { // session stoppen (auskommentierter code ist noch nicht implementiert momentan)
     // await stopSession().catch((err) => {
     //     console.error("Error stopping session:", err);
     //     res.statusCode = 500;
@@ -252,7 +264,7 @@ muscleRouter.post('/session/stop', async (req, res) => {
     res.send({ message: "stopping session successful!" });
 });
 
-muscleRouter.post('/session/pause', async (req, res) => {
+muscleRouter.post('/session/pause', async (req, res) => { // session pausieren (auskommentierter code ist noch nicht implementiert momentan)
     // await pauseSession().catch((err) => {
     //     console.error("Error pausing session", err);
     //     res.statusCode = 500;
@@ -263,7 +275,7 @@ muscleRouter.post('/session/pause', async (req, res) => {
     res.send({ message: "pausing session successful!" });
 });
 
-muscleRouter.post('/session/resume', async (req, res) => {
+muscleRouter.post('/session/resume', async (req, res) => { // session resumen (auskommentierter code ist noch nicht implementiert momentan)
     // await resumeSession().catch((err) => {
     //     console.error("Error resuming session", err);
     //     res.statusCode = 500;
@@ -274,7 +286,7 @@ muscleRouter.post('/session/resume', async (req, res) => {
     res.send({ message: "resuming session successful!" });
 });
 
-muscleRouter.post('/newExercise', async (req, res) => {
+muscleRouter.post('/newExercise', async (req, res) => { // neue Exercise starten
     await addExercise(req.body).catch((err) => {
         console.error("Error creating Exercise:", err);
         res.statusCode = 500;
@@ -285,7 +297,7 @@ muscleRouter.post('/newExercise', async (req, res) => {
     res.send({ message: "creating Exercise successful!" });
 });
 
-muscleRouter.post('/validateExercise', async (req, res) => {
+muscleRouter.post('/validateExercise', async (req, res) => { //exercise validieren
     let found = await validateExercise(req.body.name).catch((err) => {
         console.error("Error validating Exercise:", err);
         res.statusCode = 500;
@@ -296,7 +308,7 @@ muscleRouter.post('/validateExercise', async (req, res) => {
     res.send({ found: found });
 });
 
-muscleRouter.post('/sendValidationMail', async (req, res) => {
+muscleRouter.post('/sendValidationMail', async (req, res) => { //validationmail senden
     let validationCode = await validateMail(req.body.email);
     if (!validationCode) {
         res.send({validEmail: false});
